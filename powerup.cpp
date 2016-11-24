5cd22e83cb654c44d0ebcac761abf90ed2a4571d
Ben Miller <benjaminmiller229@gmail.com>
2016-11-23 23:40:19 -0500

    Update powerup.cpp

--- powerup.cpp
+++ powerup.cpp
@@ -1,7 +1,7 @@
 #include "character.h"
 #include "header.h"
 
-/* CONSTRUCTOR
+/* 
 
 Powerup::Powerup(ModelType whatType){
 	
@@ -9,6 +9,51 @@ Powerup::Powerup(ModelType whatType){
 	nodeType = model;
 	modelType = whatType;
 	isDrawable = true;
+}
+
+void drawPolygon(int a, int b, int c, int d, float v[8][3]){
+	glBegin(GL_POLYGON);
+		glVertex3fv(v[a]);
+		glVertex3fv(v[b]);
+		glVertex3fv(v[c]);
+		glVertex3fv(v[d]);
+	glEnd();
+}
+
+
+void cube(float v[8][3]){
+	drawPolygon(0, 3, 2, 1, v);
+	drawPolygon(1, 0, 4, 5, v);
+	drawPolygon(5, 1, 2, 6, v);
+	drawPolygon(2, 3, 7, 6, v);
+	drawPolygon(6, 5, 4, 7, v);
+	drawPolygon(4, 0, 3, 7, v);
+}
+
+
+void drawBox(float* c, float w, float h, float d){
+	float vertices[8][3] = { {c[0]-w/2, c[1]-h/2, c[2]+d/2},
+							 {c[0]-w/2, c[1]+h/2, c[2]+d/2},
+							 {c[0]+w/2, c[1]+h/2, c[2]+d/2},
+							 {c[0]+w/2, c[1]-h/2, c[2]+d/2}, 
+							 {c[0]-w/2, c[1]-h/2, c[2]-d/2}, 
+							 {c[0]-w/2, c[1]+h/2, c[2]-d/2}, 
+							 {c[0]+w/2, c[1]+h/2, c[2]-d/2},
+							 {c[0]+w/2, c[1]-h/2, c[2]-d/2} };
+	cube(vertices);
+}
+
+void drawSPDPU(){
+	glColor3f(0, 0, 1);
+	float origin[3] = {-3, 0, 0};
+	drawBox(origin, 1, 1, 1);
+}
+
+
+void drawShieldPU(){
+	glColor3f(0, 1, 0);
+	float origin[3] = {3, 0, 0};
+	drawBox(origin, 1, 1, 1);
 }
 
 */
