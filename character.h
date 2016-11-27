d25af82457054ad3e54dff26ee1e82f68917e3fd
Kareem Abdel Mesih <k.abdelmesih@gmail.com>
2016-11-27 00:36:30 -0500

    Update character.h

--- character.h
+++ character.h
@@ -2,12 +2,14 @@
 #define __CHARACTER_H__
 
 #include "header.h"
+#include "box.h"
 
 class Character{
 
 public:
 	Character();
 	void drawCharacter(float* pos, float* rot, float gameSpeed);
+	bool hitTest(Box box);
 
 };
 
