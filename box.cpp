d616220f667006b169a797a6833e15169039a8b1
Kareem Abdel Mesih <k.abdelmesih@gmail.com>
2016-12-06 15:11:34 -0500

    Comments

--- box.cpp
+++ box.cpp
@@ -1,13 +1,15 @@
 #include "header.h"
 #include "box.h"
 
-
+//Location that will be randomized
 float boxLoc[] = {0, 0, 0};
 
 
+//Empty Constructor
 Box::Box(){ }
 
 
+//Draws the actual polygon given a set of vertices
 void drawPolygon(int a, int b, int c, int d, float v[8][3]){
 	glBegin(GL_POLYGON);
 		glVertex3fv(v[a]);
@@ -18,6 +20,7 @@ void drawPolygon(int a, int b, int c, int d, float v[8][3]){
 }
 
 
+//Calls drawPolygon to draw a cube given a set of vertices
 void cube(float v[8][3]){
 	drawPolygon(0, 3, 2, 1, v);
 	drawPolygon(1, 0, 4, 5, v);
@@ -28,6 +31,7 @@ void cube(float v[8][3]){
 }
 
 
+//Calls cube to draw draw a cube given dimensions and providing a set of vertices
 void drawCuboid(float* c, float w, float h, float d){
 	float vertices[8][3] = { {c[0]-w/2, c[1]-h/2, c[2]+d/2},
 							 {c[0]-w/2, c[1]+h/2, c[2]+d/2},
@@ -41,18 +45,22 @@ void drawCuboid(float* c, float w, float h, float d){
 }
 
 
+//Selects a random X value for the cube
 int Box::genX(int range){
 	int x1 = rand()%(range)+1;
 	int x2 = (rand()%range + 1) * (-1);
 	return x2 + x1;
 }
 
+
+//Selects a random Z value for the cube
 int Box::genZ(int range){
 	int x = (rand()%range + 35) * (-1);
 	return x;
 }
 
 
+//Draws the green boxes
 void Box::drawCollect(int xLocation, int zLocation){
 	glColor3f(0, 1, 0);
 	boxLoc[0] = xLocation;
@@ -61,6 +69,7 @@ void Box::drawCollect(int xLocation, int zLocation){
 }
 
 
+//Draws the black boxes
 void Box::drawAvoid(int xLocation, int zLocation){
 	glColor3f(0, 0, 0);
 	boxLoc[0] = xLocation;
